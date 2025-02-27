aa3228f3cc003fa331bfc7d923101ad3bc80ef83
saheel <saheel.sarker@gmail.com>
2025-02-27 01:34:47 -0700

    remove Auth0 integration and related middleware from landing page

--- app/(landing page)/page.js
+++ app/(landing page)/page.js
@@ -2,16 +2,8 @@ import Hero from "@/components/Hero";
 import Main from "@/components/Main";
 import Pricing from "@/components/Pricing";
 import SlidingCards from "@/components/SlidingCards";
-import { auth0 } from "@/lib/auth0";
 
 export default async function Home() {
-  const session = await auth0.getSession();
-
-  if (!session) {
-    console.log("No session");
-  } else {
-    console.log(session.user.name);
-  }
 
   return (
     <Main>
