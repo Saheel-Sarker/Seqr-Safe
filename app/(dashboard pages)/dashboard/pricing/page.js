10fce0fc343f15a80ecdda3fde935a428e211d21
saheel <saheel.sarker@gmail.com>
2025-02-28 20:02:57 -0700

    Added Pricing.

---
+++ app/(dashboard pages)/dashboard/pricing/page.js
@@ -0,0 +1,9 @@
+import CenterAuthItems from '@/components/CenterAuthItems'
+import Pricing from '@/components/Pricing'
+import React from 'react'
+
+export default function page() {
+  return (
+        <Pricing redirectToPlans={true}/>
+  )
+}
