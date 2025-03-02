778e7547b0de224bf7d17627fb5573c5ff91beb2
saheel <saheel.sarker@gmail.com>
2025-03-02 05:14:30 -0700

    got rid of api folder from app

--- app/api/routes/stripe.route.js
+++
@@ -1,7 +0,0 @@
-import express from 'express';
-import { stripeWebhook } from '../controllers/stripe.controller.js';
-
-const router = express.Router();
-router.post("/stripe", stripeWebhook);
-
-export default router;
