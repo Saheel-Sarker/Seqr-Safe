a612d8bf2dcc5bc320fa34b45f466f499c0c6fec
saheel <saheel.sarker@gmail.com>
2025-03-02 05:53:10 -0700

    added some stuff to keep front and backend on same port

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import cors from 'cors';
 import cookieParser from 'cookie-parser';
 import connectDB from './config/connectDB.js';
 import passwordRoutes from './routes/password.route.js';
 import authRoutes from './routes/auth.route.js';
 import stripeRoutes from './routes/stripe.route.js';
+import path from 'path';
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -18,6 +20,13 @@ app.use(cookieParser()); // allows us to parse incoming cookies
 app.use("/api/passwords", express.json(), passwordRoutes);
 app.use("/api/auth", express.json(), authRoutes);
 app.use("/api/webhook", express.raw({ type: "application/json" }), stripeRoutes);
+
+if (process.env.NODE_ENV === "production"){
+  app.use(express.static(path.join(__dirname, "/dist")));
+  app.get("*", (req,res) => {
+    res.sendFile(path.resolve(__dirname,"dist","index.html"));
+  });
+}
 
 app.listen(PORT, () => {
   connectDB();
