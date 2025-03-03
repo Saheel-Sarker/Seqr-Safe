c49854770d2d0f686f9756b4a502a39321fd98f2
saheel <saheel.sarker@gmail.com>
2025-03-02 22:44:34 -0700

    refined the env with project

--- api/index.js
+++ api/index.js
@@ -6,24 +6,36 @@ import passwordRoutes from './routes/password.route.js';
 import authRoutes from './routes/auth.route.js';
 import stripeRoutes from './routes/stripe.route.js';
 import path from 'path';
+import cors from 'cors';
 
 const __dirname = path.resolve();
 
-dotenv.config();
+// dotenv.config();
 
 const app = express();
-const PORT = process.env.PORT || 5000;
+const backend_port = process.env.BACKEND_PORT;
+const frontend_port = process.env.FRONTEND_PORT; 
+console.log('frontend is', `http://localhost:${frontend_port}`);
+const allowedOrigins = [
+  `http://localhost:${frontend_port}`, // For local development
+  process.env.FRONTEND_VERCEL_URL,// Your Render frontend URL
+  process.env.FRONTEND_RENDER_URL
+];
 
 app.use(cookieParser()); // allows us to parse incoming cookies
+app.use(cors({
+  origin: allowedOrigins, // Replace with your actual frontend URL
+  credentials: true
+}));
 
 // Routes
 app.use("/api/passwords", express.json(), passwordRoutes);
 app.use("/api/auth", express.json(), authRoutes);
 app.use("/api/webhook", express.raw({ type: "application/json" }), stripeRoutes);
  
-app.listen(PORT, () => {
+app.listen(backend_port, () => {
   connectDB();
-  console.log(`Server started at http://localhost:${PORT}`);
+  console.log(`Server started at http://localhost:${backend_port}`);
 });
 
 export default app;
