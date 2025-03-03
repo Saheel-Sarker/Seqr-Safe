226441691b3f1980979409dde6374f91de731c9a
saheel <saheel.sarker@gmail.com>
2025-03-03 01:18:16 -0700

    production mode

--- next.config.mjs
+++ next.config.mjs
@@ -4,7 +4,7 @@ const nextConfig = {
         return [
           {
             source: '/api/:path*',
-            destination: 'http://localhost:5000/api/:path*', // your backend server URL
+            destination: process.env.BACKEND_URL || 'http://localhost:5000/api/:path*', // your backend server URL
           },
         ];
       },
