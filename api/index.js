d68e704f638634569b263c68710bdc12bea55721
saheel <saheel.sarker@gmail.com>
2025-03-28 15:55:40 -0600

    fixing seo stuff

--- api/index.js
+++ api/index.js
@@ -4,41 +4,28 @@ import connectDB from './config/connectDB.js';
 import passwordRoutes from './routes/password.route.js';
 import authRoutes from './routes/auth.route.js';
 import stripeRoutes from './routes/stripe.route.js';
-import path from 'path';
 import next from 'next';
 
-const __dirname = path.resolve();
-
 const app = express();
-const nextApp = next({ dev: false });
-const backend_port = process.env.BACKEND_PORT;
+const dev = process.env.NODE_ENV !== "production";
+const nextApp = next({ dev });
+const handle = nextApp.getRequestHandler();
+const backend_port = process.env.BACKEND_PORT || 3000;
 
 app.use(cookieParser()); 
-
 app.use("/api/passwords", express.json(), passwordRoutes);
 app.use("/api/auth", express.json(), authRoutes);
 app.use("/api/webhook", express.raw({ type: "application/json" }), stripeRoutes);
 
-console.log("going to start up frontend service");
-if (process.env.MODE_ENV === "production"){
-  console.log("starting up frontend service");
-  app.use(express.static(path.join(__dirname, '.next')));
-  app.all('*', async (req, res) => {
-    await nextApp.prepare();
-    return nextApp.getRequestHandler()(req, res);
+nextApp.prepare().then(() => {
+  app.all('*', (req, res) => {
+    return handle(req, res);
   });
-  nextApp.prepare().then(() => {
-    app.listen(backend_port, () => {
-      connectDB();
-      console.log(`Everything is serving at ${backend_port}`);
-    });
-  });
-}
-else{ 
+
   app.listen(backend_port, () => {
     connectDB();
-    console.log(`Server started at http://localhost:${backend_port}`);
+    console.log(`Server running on port ${backend_port}`);
   });
-}
+});
 
 export default app;
